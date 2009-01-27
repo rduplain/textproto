977c8d6a91bfeddc85b9f3f127826eb3960a4dfb
Ron DuPlain <ron.duplain@gmail.com>
2009-01-27 01:39:06 -0500

    Created new Android project in eclipse.

---
+++ src/com/espressolabs/android/TextProto/TextActivity.java
@@ -0,0 +1,13 @@
+package com.espressolabs.android.TextProto;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TextActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
