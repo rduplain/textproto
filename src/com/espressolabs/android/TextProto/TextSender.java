4050d680809fe78b5f43a47ad43cfd4c66032bbd
Ron DuPlain <ron.duplain@gmail.com>
2009-01-28 01:04:53 -0500

    Added basic multipart send to TextSender. Planning to add PendingIntents.

--- src/com/espressolabs/android/TextProto/TextSender.java
+++ src/com/espressolabs/android/TextProto/TextSender.java
@@ -1,9 +1,12 @@
 package com.espressolabs.android.TextProto;
 
+import java.util.ArrayList;
+
 import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.telephony.gsm.SmsManager;
 import android.util.Log;
 
 public class TextSender extends Service {
@@ -29,6 +32,15 @@ public class TextSender extends Service {
 			// Note: you'll have to pay SMS charges.
 			if(address.endsWith("xxxyyyzzzz")) {
 				Log.i("TextSender",  "echo message to " + address);
+
+				SmsManager manager = SmsManager.getDefault();
+				// Use sendMultipartTextMessage instead of sendTextMessage.
+				// This supports messages of arbitrary length.
+				ArrayList<String> parts = manager.divideMessage(message);
+				// TODO Give SmsManager a useful intent to send upon delivery.
+				// ... one for each part of the message.
+				manager.sendMultipartTextMessage(address, null, 
+						parts, null, null);
 			}
 		}
 		super.onStart(intent, startId);
