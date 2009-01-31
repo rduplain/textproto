49c116118aa3954e4397a3403f1494e9c05928be
Ron DuPlain <ron.duplain@gmail.com>
2009-01-31 15:51:39 -0500

    Cleaned up TextSender/Router for cleaner separation, comments, spaces for tabs.

--- src/com/espressolabs/android/TextProto/TextRouter.java
+++ src/com/espressolabs/android/TextProto/TextRouter.java
@@ -6,30 +6,70 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 
+/**
+ * Provides a hub for messages.
+ * Use an Intent with the <code>startService</code> method.
+ * Provide intent extras for 'sender', 'address', and 'message' as Strings.
+ * 
+ * sender: message origin
+ * address: message destination
+ * message: message content
+ * 
+ * If address is empty, sender is used as the address.
+ * 
+ * This class should be modified to match the needs of its enclosing project.
+ * As is, this class provides a demo and an entry point for managing message.
+ * Once called, a message is given to TextSender based on a hard-coded rule.
+ */
 public class TextRouter extends Service {
+    private final String tag = "TextRouter"; // used for logs
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public IBinder onBind(Intent intent) {
+        // Everything is currently in onStart.
+        return null;
+    }
 
-	@Override
-	public void onStart(Intent intent, int startId) {
-		Bundle extras = intent.getExtras();
-		String sender = extras.getString("sender");
-		String message = extras.getString("message");
+    @Override
+    public void onStart(Intent intent, int startId) {
+        // Get the sender and message from the intent extras.
+        Bundle extras = intent.getExtras();
+        String sender = extras.getString("sender");
+        String address = extras.getString("address");
+        String message = extras.getString("message");
 
-		// Act on the message, if it's not empty.
-		if(message != null && message.length() > 0) {
-			Log.i("TextRouter",  sender + ": " + message);
+        if(address == null || address.length() <= 0) {
+            address = sender;
+        }
 
-			// For now, pass the message along to the TextSender.
-			Intent newIntent = new Intent(this, TextSender.class);
-			newIntent.putExtra("address", sender);
-			newIntent.putExtra("message", message);
-			this.startService(newIntent);
-		}
-		super.onStart(intent, startId);
-	}
+        // Act on non-empty messages.
+        if(message != null && message.length() > 0) {
+            // HERE Do something interesting with the message.
+            // HERE Replace this block of code with respect to project needs.
+
+            // For now, pass the message along to the TextSender.
+            // For a demo, simply log the message and send it a test phone.
+            Log.i(tag,  address + ": " + message);
+
+            // Send the message out if it matches the hardcoded number below.
+            // Replace xxxyyyzzzz with number of a spare SMS-capable phone.
+            // Prepend message with some text, especially to verify multipart.
+            // i.e. send a max length message from test phone, receive 2 SMSs.
+            // NOTE SMS charges will apply.
+            if(address.endsWith("xxxyyyzzzz")) {
+                Log.i(tag,  "send message to " + address);
+                message = "(Test) " + sender + " said: " + message;
+
+                // TextSender requires intent extras 'address' and 'message'.
+                Intent newIntent = new Intent(this, TextSender.class);
+                newIntent.putExtra("address", address);
+                newIntent.putExtra("message", message);
+                this.startService(newIntent);
+            } else {
+                // Log that we're doing nothing, for clarity in the log.
+                Log.i(tag, "doing nothing with message from " + address);
+            }
+        }
+        super.onStart(intent, startId);
+    }
 }
--- src/com/espressolabs/android/TextProto/TextSender.java
+++ src/com/espressolabs/android/TextProto/TextSender.java
@@ -10,55 +10,63 @@ import android.os.IBinder;
 import android.telephony.gsm.SmsManager;
 import android.util.Log;
 
+/**
+ * Sends SMS messages. Use an Intent with the <code>startService</code> method.
+ * Provide intent extras for 'address' and 'message' as Strings.
+ * The given message can be any length. Messages exceeding the max SMS length
+ * are sent in multipart.
+ * 
+ * Tells SmsManager to call TextCallback after handling the SMS message.
+ * 
+ * Service will fail if no address or message is given.
+ */
 public class TextSender extends Service {
+    private final String tag = "TextSender"; // used for logs
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public IBinder onBind(Intent intent) {
+        // Everything is currently in onStart.
+        return null;
+    }
 
-	@Override
-	public void onStart(Intent intent, int startId) {
-		Bundle extras = intent.getExtras();
-		String address = extras.getString("address");
-		String message = extras.getString("message");
+    @Override
+    public void onStart(Intent intent, int startId) {
+        // Get the address and message from the intent extras.
+        Bundle extras = intent.getExtras();
+        String address = extras.getString("address");
+        String message = extras.getString("message");
 
-		// Act on the message, if it's not empty.
-		if(message != null && message.length() > 0) {
-			Log.i("TextSender",  address + ": " + message);
+        // Act on non-empty messages.
+        if(message != null && message.length() > 0) {
+            Log.i(tag,  address + ": " + message);
 
-			// Test with a loopback, based on target.
-			// Replace xxxyyyzzzz with number of your spare SMS-capable phone.
-			// Note: you'll have to pay SMS charges.
-			if(address.endsWith("xxxyyyzzzz")) {
-				Log.i("TextSender",  "echo message to " + address);
-				message = "(Test) You said: " + message;
-
-				SmsManager manager = SmsManager.getDefault();
-				// Use sendMultipartTextMessage instead of sendTextMessage.
-				// This supports messages of arbitrary length.
-				ArrayList<String> parts = manager.divideMessage(message);
-				// Give SmsManager a useful intent to send upon delivery.
-				// ... one for each part of the message.
-				ArrayList<PendingIntent> pendingIntents = 
-					new ArrayList<PendingIntent>();
-				for(int i = 0; i < parts.size(); ++i) {
-					Intent newIntent = new Intent(this, TextCallback.class);
-					newIntent.putExtra("address", address);
-					newIntent.putExtra("message", parts.get(i));
-					pendingIntents.add(PendingIntent.getBroadcast(this, 0,
-							newIntent, 0));
-				}
-				try {
-					// HERE Provide sentIntents, deliveryIntents, or both?
-					manager.sendMultipartTextMessage(address, null, parts,
-							pendingIntents, null);
-				} catch(Exception e) {
-					Log.e("TextSender.onStart", "failed to send", e);
-		    	}
-			}
-		}
-		super.onStart(intent, startId);
-	}
+            // Get the default SmsManager from the system.
+            SmsManager manager = SmsManager.getDefault();
+            // Use sendMultipartTextMessage instead of sendTextMessage.
+            // This supports messages of arbitrary length.
+            // Before using sendMultipartTextMessage, use divideMessage.
+            ArrayList<String> parts = manager.divideMessage(message);
+            // Give SmsManager a useful intent to use after completion.
+            // ... one for each part of the message.
+            ArrayList<PendingIntent> pendingIntents = 
+                new ArrayList<PendingIntent>();
+            for(int i = 0; i < parts.size(); ++i) {
+                // Use the TextCallback class for the PendingIntents.
+                Intent newIntent = new Intent(this, TextCallback.class);
+                newIntent.putExtra("address", address);
+                newIntent.putExtra("message", parts.get(i));
+                pendingIntents.add(PendingIntent.getBroadcast(this, 0,
+                        newIntent, 0));
+            }
+            // sendMultipartTextMessage throws IllegalArgumentException
+            try {
+                // HERE Provide sentIntents, deliveryIntents, or both?
+                manager.sendMultipartTextMessage(address, null, parts,
+                        pendingIntents, null);
+            } catch(Exception e) {
+                Log.e(tag, "failed to send message", e);
+            }
+        }
+        super.onStart(intent, startId);
+    }
 }
